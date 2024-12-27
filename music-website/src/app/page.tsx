40e4dd61b5b384b23a65fea658485dc96b36350c
Utkarsh Kumar Upadhyay <ukupadhyay6789@gmail.com>
2024-12-27 23:58:38 +0530

    Card section added

--- music-website/src/app/page.tsx
+++ music-website/src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import MainSection from "@/components/MainSection";
 import { main } from "framer-motion/client";
 import Image from "next/image";
@@ -7,6 +8,7 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       {/* <h1 className="text-2xl text-center">Tara aur Tarini</h1> */}
       <MainSection/>
+      <FeaturedCourses />
     </main>
   );
 }
