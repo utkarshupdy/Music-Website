0b0fcc09512609c485c0ef2323af4683f48a43e9
Utkarsh Kumar Upadhyay <ukupadhyay6789@gmail.com>
2024-12-27 22:06:08 +0530

    Main Section added

--- music-website/src/app/layout.tsx
+++ music-website/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/>
+        <div className="relative w-full flex items-center justify-center" ><Navbar/></div>
         {children}
       </body>
     </html>
