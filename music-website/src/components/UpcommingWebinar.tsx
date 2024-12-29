150c841d5222f9ee537d87f7410eba1e4dd292f5
Utkarsh Kumar Upadhyay <ukupadhyay6789@gmail.com>
2024-12-30 02:59:54 +0530

    minor bugs

--- music-website/src/components/UpcommingWebinar.tsx
+++ music-website/src/components/UpcommingWebinar.tsx
@@ -29,7 +29,7 @@ function UpcomingWebinars() {
         </div>
 
         <div className="mt-10 text-center">
-          <Link href={"/all-courses"}
+          <Link href={"/webinar"}
           className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"
           >
             View All webinars
